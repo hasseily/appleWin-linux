154e5c9a9d7bccaace98706387d95fc724971492
Jamiras <jamirasmt@gmail.com>
2024-11-28 20:58:11 -0700

    tabs->spaces

--- source/linux/soundbuffer.h
+++ source/linux/soundbuffer.h
@@ -9,44 +9,44 @@
 class SoundBuffer : public SoundBufferBase
 {
 private:
-	std::vector<uint8_t> mySoundBuffer;
+  std::vector<uint8_t> mySoundBuffer;
 
-	size_t myPlayPosition = 0;
-	size_t myWritePosition = 0;
-	WORD myStatus = 0;
-	LONG myVolume = 0;
+  size_t myPlayPosition = 0;
+  size_t myWritePosition = 0;
+  WORD myStatus = 0;
+  LONG myVolume = 0;
 
-	// updated by the callback
-	std::atomic_size_t myNumberOfUnderruns;
-	std::mutex myMutex;
+  // updated by the callback
+  std::atomic_size_t myNumberOfUnderruns;
+  std::mutex myMutex;
 
 public:
-	size_t myBufferSize;
-	size_t mySampleRate;
-	size_t myChannels;
-	size_t myBitsPerSample;
+  size_t myBufferSize;
+  size_t mySampleRate;
+  size_t myChannels;
+  size_t myBitsPerSample;
 
-	HRESULT Init(DWORD dwFlags, DWORD dwBufferSize, DWORD nSampleRate, int nChannels, LPCSTR pDevName) override;
-	HRESULT Release() override;
+  HRESULT Init(DWORD dwFlags, DWORD dwBufferSize, DWORD nSampleRate, int nChannels, LPCSTR pDevName) override;
+  HRESULT Release() override;
 
-	HRESULT SetCurrentPosition(DWORD dwNewPosition) override;
-	HRESULT GetCurrentPosition(LPDWORD lpdwCurrentPlayCursor, LPDWORD lpdwCurrentWriteCursor) override;
+  HRESULT SetCurrentPosition(DWORD dwNewPosition) override;
+  HRESULT GetCurrentPosition(LPDWORD lpdwCurrentPlayCursor, LPDWORD lpdwCurrentWriteCursor) override;
 
-	HRESULT Lock(DWORD dwWriteCursor, DWORD dwWriteBytes, LPVOID* lplpvAudioPtr1, DWORD* lpdwAudioBytes1, LPVOID* lplpvAudioPtr2, DWORD* lpdwAudioBytes2, DWORD dwFlags) override;
-	virtual HRESULT Unlock(LPVOID lpvAudioPtr1, DWORD dwAudioBytes1, LPVOID lpvAudioPtr2, DWORD dwAudioBytes2) override;
+  HRESULT Lock(DWORD dwWriteCursor, DWORD dwWriteBytes, LPVOID* lplpvAudioPtr1, DWORD* lpdwAudioBytes1, LPVOID* lplpvAudioPtr2, DWORD* lpdwAudioBytes2, DWORD dwFlags) override;
+  virtual HRESULT Unlock(LPVOID lpvAudioPtr1, DWORD dwAudioBytes1, LPVOID lpvAudioPtr2, DWORD dwAudioBytes2) override;
 
-	virtual HRESULT Stop() override;
-	virtual HRESULT Play(DWORD dwReserved1, DWORD dwReserved2, DWORD dwFlags) override;
+  virtual HRESULT Stop() override;
+  virtual HRESULT Play(DWORD dwReserved1, DWORD dwReserved2, DWORD dwFlags) override;
 
-	virtual HRESULT SetVolume(LONG lVolume) override;
-	HRESULT GetVolume(LONG* lplVolume) override;
+  virtual HRESULT SetVolume(LONG lVolume) override;
+  HRESULT GetVolume(LONG* lplVolume) override;
 
-	HRESULT GetStatus(LPDWORD lpdwStatus) override;
-	HRESULT Restore() override;
+  HRESULT GetStatus(LPDWORD lpdwStatus) override;
+  HRESULT Restore() override;
 
-	DWORD Read(DWORD dwReadBytes, LPVOID* lplpvAudioPtr1, DWORD* lpdwAudioBytes1, LPVOID* lplpvAudioPtr2, DWORD* lpdwAudioBytes2);
-	DWORD GetBytesInBuffer();
-	size_t GetBufferUnderruns() const;
-	void ResetUnderruns();
-	double GetLogarithmicVolume() const;  // in [0, 1]
+  DWORD Read(DWORD dwReadBytes, LPVOID* lplpvAudioPtr1, DWORD* lpdwAudioBytes1, LPVOID* lplpvAudioPtr2, DWORD* lpdwAudioBytes2);
+  DWORD GetBytesInBuffer();
+  size_t GetBufferUnderruns() const;
+  void ResetUnderruns();
+  double GetLogarithmicVolume() const;  // in [0, 1]
 };
